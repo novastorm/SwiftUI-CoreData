4a149e29a2262bdf8af56b6f4bd4a68880dc3482
Adland Lee <adland.lee@gmail.com>
2019-06-05 09:50:10 -0700

    Initial Commit

---
+++ SwiftUI+CoreData/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI+CoreData
+//
+//  Created by Bahlreighn on 6/5/19.
+//  Copyright © 2019 Bahlreighn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
